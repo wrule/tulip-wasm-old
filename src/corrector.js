317ee134f4dec398f75eb1001b4e764e6ca33d41
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-19 22:48:22 +0800

    ts转化成为js

--- src/corrector.js
+++ src/corrector.js
@@ -1,20 +1,18 @@
 import fs from 'fs';
-import path from 'path';
-import { Indicator } from './indicator';
 
 const tasks = {
-  'add': (indicator: Indicator) => indicator.input_names = ['real1', 'real2'],
-  'sub': (indicator: Indicator) => indicator.input_names = ['real1', 'real2'],
-  'mul': (indicator: Indicator) => indicator.input_names = ['real1', 'real2'],
-  'div': (indicator: Indicator) => indicator.input_names = ['real1', 'real2'],
-  'crossany': (indicator: Indicator) => indicator.input_names = ['real1', 'real2'],
-  'crossover': (indicator: Indicator) => indicator.input_names = ['real1', 'real2'],
-  'var': (indicator: Indicator) => indicator.name = '_var',
-}
+  'add': (indicator) => indicator.input_names = ['real1', 'real2'],
+  'sub': (indicator) => indicator.input_names = ['real1', 'real2'],
+  'mul': (indicator) => indicator.input_names = ['real1', 'real2'],
+  'div': (indicator) => indicator.input_names = ['real1', 'real2'],
+  'crossany': (indicator) => indicator.input_names = ['real1', 'real2'],
+  'crossover': (indicator) => indicator.input_names = ['real1', 'real2'],
+  'var': (indicator) => indicator.name = '_var',
+};
 
 function main() {
   const filepath = process.argv[2];
-  const indicators: Indicator[] = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+  const indicators = JSON.parse(fs.readFileSync(filepath, 'utf8'));
   Object.entries(tasks).forEach(([name, func]) => {
     const indicator = indicators.find((indicator) => indicator.name === name);
     if (indicator) {
