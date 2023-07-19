23477c978dfb1f8dcb4117d98355f34f53fa6c06
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-19 22:06:25 +0800

    indicator.ts复制过来

---
+++ src/indicator.ts
@@ -0,0 +1,14 @@
+
+export
+interface Indicator {
+  index: number;
+  name: string;
+  full_name: string;
+  type: number;
+  inputs: number;
+  options: number;
+  outputs: number;
+  input_names: string[];
+  option_names: string[];
+  output_names: string[];
+}
