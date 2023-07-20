3a9f1ac6fb57402da639351a51f2c47a91a287cc
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-20 22:55:57 +0800

    双引号替换为单引号

--- src/dev.ts
+++ src/dev.ts
@@ -2,15 +2,15 @@ import * as tulip from './index';
 export * from './index';
 
 const isBrowser =
-  typeof window !== "undefined" && typeof window.document !== "undefined";
+  typeof window !== 'undefined' && typeof window.document !== 'undefined';
 
 const isNode =
-  typeof process !== "undefined" &&
+  typeof process !== 'undefined' &&
   process.versions != null &&
   process.versions.node != null;
 
 const isWebWorker =
-  typeof self === "object" &&
+  typeof self === 'object' &&
   self.constructor &&
-  self.constructor.name === "DedicatedWorkerGlobalScope";
+  self.constructor.name === 'DedicatedWorkerGlobalScope';
 const env: any = null;
