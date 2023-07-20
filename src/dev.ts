5181d07cc8c351bbd7ad4c8413e0976e9a12fef0
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-20 22:54:41 +0800

    dev的入口修改

---
+++ src/dev.ts
@@ -0,0 +1,16 @@
+import * as tulip from './index';
+export * from './index';
+
+const isBrowser =
+  typeof window !== "undefined" && typeof window.document !== "undefined";
+
+const isNode =
+  typeof process !== "undefined" &&
+  process.versions != null &&
+  process.versions.node != null;
+
+const isWebWorker =
+  typeof self === "object" &&
+  self.constructor &&
+  self.constructor.name === "DedicatedWorkerGlobalScope";
+const env: any = null;
