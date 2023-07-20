136bcda5b7f8b89fa2cae64344bc84d4935058ad
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-20 15:19:48 +0800

    新的测试代码

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import { tulip, run_alone } from './meta';
 
 function main() {
   console.log('你好，世界');
+  const outputs = run_alone(tulip, 48, [[1, 2, 3, 4, 5, 6]], [3], 1);
+  console.log(outputs);
 }
 
 main();
