46f4d379431ad5aee09ec9757f84c919d7ca1686
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-20 22:14:18 +0800

    新的测试代码

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import { tulip } from './meta';
+import { tulip, run_alone } from './meta';
 
 async function main() {
   console.log('你好，世界');
-  const a = await tulip();
+  const a = await tulip;
   console.log(a._new_task(1, 100));
   console.log(a._new_task(1, 100));
   console.log(a._new_task(1, 100));
+  console.log(await run_alone(tulip, 72, [[1, 2, 3, 5]], [2], 1));
 }
 
 main();
