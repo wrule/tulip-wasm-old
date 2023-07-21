c1b2600a3e3c9482b295cce91ea99acda5707c9b
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-21 13:40:51 +0800

    GlobalSet方法

--- src/utils.ts
+++ src/utils.ts
@@ -12,3 +12,11 @@ const IsWebWorker = () =>
   typeof self === 'object' &&
   self.constructor &&
   self.constructor.name === 'DedicatedWorkerGlobalScope';
+
+const GlobalSet = (key: string, value: any) => {
+  let env: any = null;
+  if (IsBrowser()) env = window;
+  if (IsNode()) env = global;
+  if (IsWebWorker()) env = self;
+  if (env) env[key] = value;
+};
