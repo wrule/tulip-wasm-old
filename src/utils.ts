28111c9594b1aab8f0647987b2c78efa421cae70
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-21 13:30:01 +0800

    utils内环境判断功能函数化

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,14 @@
 
-const isBrowser =
-  typeof window !== 'undefined' && typeof window.document !== 'undefined';
+const IsBrowser = () =>
+  typeof window !== 'undefined' &&
+  typeof window.document !== 'undefined';
 
-const isNode =
+const IsNode = () =>
   typeof process !== 'undefined' &&
   process.versions != null &&
   process.versions.node != null;
 
-const isWebWorker =
+const IsWebWorker = () =>
   typeof self === 'object' &&
   self.constructor &&
   self.constructor.name === 'DedicatedWorkerGlobalScope';
