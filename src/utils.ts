bdd83cef0fe0a60a67a1819950e3e95a6f579784
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-21 13:42:52 +0800

    GetEnv方法

--- src/utils.ts
+++ src/utils.ts
@@ -13,6 +13,14 @@ const IsWebWorker = () =>
   self.constructor &&
   self.constructor.name === 'DedicatedWorkerGlobalScope';
 
+const GetEnv = () => {
+  let env: any = null;
+  if (IsBrowser()) env = window;
+  if (IsNode()) env = global;
+  if (IsWebWorker()) env = self;
+  return env;
+};
+
 const GlobalSet = (key: string, value: any) => {
   let env: any = null;
   if (IsBrowser()) env = window;
