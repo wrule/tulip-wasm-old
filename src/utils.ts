a9f232eb29ec08ddd08951b2169cac5c42666009
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-21 13:51:52 +0800

    utils内变量添加导出

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,22 @@
 
+export
 const IsBrowser =
   typeof window !== 'undefined' &&
   typeof window.document !== 'undefined';
 
+export
 const IsNode =
   typeof process !== 'undefined' &&
   process.versions != null &&
   process.versions.node != null;
 
+export
 const IsWebWorker =
   typeof self === 'object' &&
   self.constructor &&
   self.constructor.name === 'DedicatedWorkerGlobalScope';
 
+export
 const Global = (() => {
   let env: any = null;
   if (IsBrowser) env = window;
