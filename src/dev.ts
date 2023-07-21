10b3f624abf7e81a429bf15abbc03b7f639d2dab
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-21 17:03:35 +0800

    测试性能

--- src/dev.ts
+++ src/dev.ts
@@ -2,7 +2,10 @@ import * as tulip from '.';
 
 async function main() {
   await tulip.init();
-  console.log(tulip.sma([1, 2, 3, 4], 3));
+  const list = Array(10000000).fill(0).map(() => Math.random() * 1000);
+  const old_time = Date.now();
+  tulip.sma(list, 3);
+  console.log(Date.now() - old_time);
 }
 
 main();
