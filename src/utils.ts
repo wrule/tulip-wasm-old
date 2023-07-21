48e929939d9ae0d9baa1636cb64d61d247f182c9
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-21 13:26:33 +0800

    utils复制部分代码

---
+++ src/utils.ts
@@ -0,0 +1,13 @@
+
+const isBrowser =
+  typeof window !== 'undefined' && typeof window.document !== 'undefined';
+
+const isNode =
+  typeof process !== 'undefined' &&
+  process.versions != null &&
+  process.versions.node != null;
+
+const isWebWorker =
+  typeof self === 'object' &&
+  self.constructor &&
+  self.constructor.name === 'DedicatedWorkerGlobalScope';
